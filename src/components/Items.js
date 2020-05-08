4aaf4cc9920d6648b0a4de14da0d4be1f1955933
jdnav <josedaniel.navarro@edu.uah.es>
2020-05-08 22:25:13 +0200

    Items panel displays a table

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
 function Items(props) {
-    return (
-        <div>
-            Items!
-        </div>
-    );
+  return (
+    <Fragment>
+      <h2 className="text-center my-5">Items list</h2>
+
+      <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </Fragment>
+  );
 }
 
 export default Items;
